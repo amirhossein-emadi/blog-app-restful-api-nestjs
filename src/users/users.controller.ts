d4270bac689aeacfa4946b0bd990937e5b45118f
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-08-10 17:28:19 +0330

    feat: implement the getUsersById method for users controller with Params, Query and Body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-
   @Get()
-  public getUsers() {
+  public getUsers(@Query() query: any) {
+    console.log(query);
     return 'Getting all users coming soon!';
   }
 
   @Post()
-  public createUsers() {
+  public createUsers(@Body() request: any) {
+    console.log(request);
     return 'Creating a user coming soon!';
   }
 
+  @Get('/:id{/:city}')
+  public getUsersById(@Param() params: any) {
+    console.log(params);
+    return 'Getting the user by id coming soon!';
+  }
 }
